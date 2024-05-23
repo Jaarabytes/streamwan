75f228477b66482dcab291f8dc03dcf6729dcdd7
Jaarabytes <xavierandole78@gmail.com>
2024-05-23 14:18:23 +0300

    refactor: done with frontend
    ui is mostly done. come back for animation

--- app/ui/Finalize.tsx
+++ app/ui/Finalize.tsx
@@ -1,19 +1,35 @@
+'use client'
 import Link from "next/link"
-
+import { useState, useEffect } from "react"
 
 export default function Finalize () {
     const builtFor = ["families", "companies", "households"]
+    const [ currentIndex, setCurrentIndex ] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => ( prevIndex + 1 ) %  builtFor.length)
+        }, 2000);
+
+        return () => clearInterval(interval)
+    }, [])
     // add relevant href links
     return (
         <>
             <div className='sm:my-48 px-[20%]'>
-                {builtFor.map((element, index) => (
-                    // Add carousel animation similar to wazo but moves horizontally
-                    <span key={index}>
-                        {/* Made for {element}. add a function (possibly a for loop) */}
-                    </span>
-                ))}
                 <h1 className="text-3xl mx-5 text-green-900 font-bold">Wi-Fight us?</h1>
+                {/* {builtFor.map((element, index) => (
+                    // Add carousel animation similar to wazo but moves horizontally
+                    <div
+                    className="flex items-center whitespace no-wrap transition-transform duration-500"
+                    style={{transform: `translateX(-${currentIndex * 100}%)`}}>
+                    <span key={index}
+                    style={{transform: `translateX(${(index - currentIndex) * 100}%)`}}
+                    >
+                        {builtFor[index]}
+                    </span>
+                    </div>
+                ))} */}
                 <div className="sm:text-center sm:flex sm:my-16 px-5">
                     <Link href={`/trial`}>
                         <button
--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -9,24 +9,24 @@ export default function Footer() {
       <h1 className="text-xl font-extrabold my-5">StreamWan</h1>
       <div>
         <h3 className="text-lg font-bold text-slate-300 mt-5">PRODUCT:</h3>
-        <Link target="_self" rel="noreferrer" href={`/trial`}  className="block my-3">Pricing</Link>
-        <Link target="_self" rel="noreferrer" href={`/faqs`}   className="block my-3">FAQs</Link>
+        <Link target="_blank" rel="noreferrer" href={`/trial`}  className="block my-3">Pricing</Link>
+        <Link target="_blank" rel="noreferrer" href={`/faqs`}   className="block my-3">FAQs</Link>
       </div>
       <div>
         <h2 className="text-lg font-bold text-slate-300 mt-5">ABOUT:</h2>
-        <Link target="_self" rel="noreferrer" href={`/privacy`} className="block my-3">Privacy</Link>
-        <Link target="_self" rel="noreferrer" href={`/faqs`} className="block my-3">Company</Link>
+        <Link target="_blank" rel="noreferrer" href={`/privacy`} className="block my-3">Privacy</Link>
+        <Link target="_blank" rel="noreferrer" href={`/faqs`} className="block my-3">Company</Link>
       </div>
       <div className="block">
         <h1 className="text-lg font-bold text-slate-300 mt-5">CONTACT:</h1>
-        <Link target="_self" rel="noreferrer" href="https://twitter.com/jaarabytes" className="block my-3">Twitter</Link>
-        <Link target="_self" rel="noreferrer" href="mailto:xavierandole@gmail.com" className="block my-3">Mail</Link>
-        <Link target="_self" rel="noreferrer" href="https://github.com/jaarabytes" className="block my-3">Github</Link>
+        <Link target="_blank" rel="noreferrer" href="https://twitter.com/jaarabytes" className="block my-3">Twitter</Link>
+        <Link target="_blank" rel="noreferrer" href="mailto:xavierandole@gmail.com" className="block my-3">Mail</Link>
+        <Link target="_blank" rel="noreferrer" href="https://github.com/jaarabytes" className="block my-3">Github</Link>
       </div>
       <div className="block">
         <h1 className="text-lg font-bold text-slate-300 mt-5">LEGAL:</h1>
-        <Link target="_self" rel="noreferrer" href={`/terms`}  className="block my-3">Terms of service</Link>
-        <Link target="_self" rel="noreferrer" href={`/privacy`}  className="block my-3">Privacy policy</Link>
+        <Link target="_blank" rel="noreferrer" href={`/terms`}  className="block my-3">Terms of service</Link>
+        <Link target="_blank" rel="noreferrer" href={`/privacy`}  className="block my-3">Privacy policy</Link>
       </div>
       </div>
       <footer className="text-center bg-green-900 text-slate-400 pb-10">© Copyright 2024 StreamWan. All Rights Reserved.</footer>
