54c6c0c08c330c26606e071661470cdd831982d4
Jaarabytes <xavierandole78@gmail.com>
2024-05-23 16:14:45 +0300

    hehe git errors you know.

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // import type { NextAuthConfig } from 'next-auth';
 
 // export const authConfig = {
@@ -28,26 +29,30 @@
 // } satisfies NextAuthConfig;
 =======
 import type { NextAuthConfig } from 'next-auth';
+=======
+// import type { NextAuthConfig } from 'next-auth';
+>>>>>>> 0bb4b17 (hehe git errors you know.)
 
-export const authConfig = {
-    pages : {
-        signIn: "/login",
-    },
-    callbacks: {
-        authorized ({ auth, request: { nextUrl }}) {
-            const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            if (isOnDashboard) {
-                if (isLoggedIn) {
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            else if (isLoggedIn) {
-                return Response.redirect(new URL('/dashboard', nextUrl));
+// export const authConfig = {
+//     pages : {
+//         signIn: "/login",
+//     },
+//     callbacks: {
+//         authorized ({ auth, request: { nextUrl }}) {
+//             const isLoggedIn = !!auth?.user;
+//             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+//             if (isOnDashboard) {
+//                 if (isLoggedIn) {
+//                     return true;
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+//             else if (isLoggedIn) {
+//                 return Response.redirect(new URL('/dashboard', nextUrl));
 
+<<<<<<< HEAD
             }
             return true;
         }
@@ -55,3 +60,11 @@ export const authConfig = {
     providers: []
 } satisfies NextAuthConfig;
 >>>>>>> 028dfa3 (feat: add auth)
+=======
+//             }
+//             return true;
+//         }
+//     },
+//     providers: []
+// } satisfies NextAuthConfig;
+>>>>>>> 0bb4b17 (hehe git errors you know.)
