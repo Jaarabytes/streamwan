8653b26ad03c6c53e1da03ca80b93d4e053a347e
Jaarabytes <xavierandole78@gmail.com>
2024-05-23 08:20:10 +0300

    feat: add auth
    add auth using nextjs and typescript middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import type { NextAuthConfig } from 'next-auth';
 
 // export const authConfig = {
@@ -25,3 +26,32 @@
 //     },
 //     providers: []
 // } satisfies NextAuthConfig;
+=======
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+    pages : {
+        signIn: "/login",
+    },
+    callbacks: {
+        authorized ({ auth, request: { nextUrl }}) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+
+            }
+            return true;
+        }
+    },
+    providers: []
+} satisfies NextAuthConfig;
+>>>>>>> 028dfa3 (feat: add auth)
