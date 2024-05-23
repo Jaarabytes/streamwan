ff8f2c5448ec3a57c149ec951a7d617bd461ae50
Jaarabytes <xavierandole78@gmail.com>
2024-05-23 10:06:08 +0300

    refactor: add links
    add `Link` component to buttons to allow trail and sign ups

--- app/ui/Intro.tsx
+++ app/ui/Intro.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Intro(){
     return(
         <>
@@ -6,27 +8,31 @@ export default function Intro(){
                 {/* <img src="https://jomeista.github.io/streamwan/img/Homefibre.png" /> */}
             {/* </div> */}
             <div className="w-full">
-                <h1 className="md:text-[80px] text-[40px] text-center text-green-900 font-extrabold my-5">Fast internet For the Family</h1>
+                <h1 className="md:text-[80px] text-[40px] text-center text-green-900 font-extrabold my-5">Fast internet For the Family!</h1>
                 <p className="md:text-[20px] text-[20px] text-center text-green-700 my-16">Be it personal, family or a company, StreamWan provides <br /><span className="font-bold"> blazingly fast internet services
                 </span> at favorable prices.</p>
                 <br />
                 <div className="sm:text-center sm:flex sm:justify-center sm:my-16 px-5">
-                    <button
-                     className="block w-full sm:w-[150px] text-lg text-green-900 rounded-xl border-2 border-green-900 hover:bg-slate-200 transition duration:500"
-                     style={{
-                        paddingBlock: "10px",
-                        paddingInline: "30px"
-                    }}
-                    //  href=''
-                     >Get demo</button>
-                    <button 
-                    className="block w-full sm:w-[150px] sm:mx-5 bg-green-900 text-lg rounded-xl text-white hover:bg-green-800 transition duration:500"
-                    style={{
-                        paddingBlock: "10px",
-                        paddingInline: "30px"
-                    }}
-                    // href=''
-                    >Buy me!</button>
+                    <Link href={`/trial`}>
+                        <button
+                        className="block w-full sm:w-[150px] text-lg text-green-900 rounded-xl border-2 border-green-900 hover:bg-slate-100/70 transition duration:500"
+                        style={{
+                            paddingBlock: "10px",
+                            paddingInline: "30px"
+                        }}
+                        >Get demo</button>
+                    </Link>
+
+                    <Link href={`/sign_in`}>
+                        <button 
+                        className="block w-full sm:w-[150px] sm:mx-5 bg-green-900 text-lg rounded-xl text-white hover:bg-green-800 transition duration:500"
+                        style={{
+                            paddingBlock: "10px",
+                            paddingInline: "30px"
+                        }}
+                        // href=''
+                        >Buy me!</button>
+                    </Link>
                 </div>
             </div>
         </div>
