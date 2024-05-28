b6443d9c013c6e32e0491d3219ebdc85ed6a3529
Jaarabytes <xavierandole78@gmail.com>
2024-05-28 12:23:13 +0300

    feat: implement sign up
    add sign up/register feature

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,15 +5,8 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-
-export type State = {
-    errors? : {
-        customerId? : string[],
-        amount? : string[],
-        state? : string[],
-    };
-    message? : string | null;
-}
+import bcrypt from 'bcrypt' 
+import { v4 as uuidv4 } from 'uuid';
 
 export async function authenticate (prevState : string | undefined , formData: FormData) {
     try {
@@ -32,98 +25,39 @@ export async function authenticate (prevState : string | undefined , formData: F
     }
 }
 
-const formSchema = z.object({
-    id: z.string(),
-    customerId: z.string({
-        invalid_type_error: "Please select a customer"
-    }),
-    amount: z.coerce.number().gt(0, {
-        message: "Please enter an amount greater than $0"
-    }),
-    status: z.enum(['pending', 'paid'], {
-        invalid_type_error: "Please select an invoice status"
-    }),
-    date: z.string()
+const RegisterUser = z.object({
+    email: z.string({invalid_type_error: "Please enter a valid email address"}),
+    password: z.string({invalid_type_error: "Please enter a valid password"})
 })
 
-const CreateInvoice = formSchema.omit({date: true, id: true});
+export async function register (prevState: string | undefined, formData: FormData) {
+    const validatedFields = RegisterUser.safeParse({email: formData.get('email'), password: formData.get('password')});
+    if (!validatedFields.success) return "Missing fields, failed to create account";
+    const { email, password } = validatedFields.data
 
-export async function createInvoice(prevState: State , formData: FormData)  {
-    const validatedFields = CreateInvoice.safeParse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-
-    if (!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Missing fields. Failed to create invoice'
-        }
-    }
-
-    const {customerId, amount, status} = validatedFields.data;
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-    try {
-        await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-        `;
-    } catch (error) {
-        return {
-            message: "Database error: Failed to create invoice"
-        }
-    }
-
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-}
-
-const UpdateInvoice = formSchema.omit({date: true, id: true});
-
-export async function updateInvoice(id: string, prevState: State, formData: FormData)  {
-    const validatedFields = UpdateInvoice.safeParse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-
-    if(!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors,
-            message: "Missing fields. Failed to update invoice"
-        }
-    }
-
-    const {customerId, amount, status} = validatedFields.data;
-    const amountInCents = amount * 100;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const id = uuidv4()
 
     try {
-        await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status= ${status}
-        WHERE id = ${id}
-        `;
-    } catch (error) {
-        return {
-            Message: "Database error: Failed to update Invoices"
+        const emailExists = await sql`
+        SELECT 1 FROM users WHERE email = ${email}
+        `
+        if (!emailExists.rows.length) {
+            await sql`
+            INSERT INTO users (id, email, password) IF users.email NOT EXISTS
+            VALUES (${id}, ${email}, ${hashedPassword})
+            `
+            console.log("User created successfully")
+            await signIn('credentials', formData);
         }
-    }
-
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-}
-
-export async function deleteInvoice (id: string) {
-    // throw new Error ("Failed to delete new Invoice")
-    try {
-        await sql`DELETE FROM INVOICES WHERE id = ${id}`
-    }
-    catch (error) {
-        return {
-            Message: "Database Error: Failed to delete invoice"
+        else {
+            console.error("User already exists")
         }
+        
     }
-    revalidatePath('dashboard/invoices');
+    catch (err) {
+        console.error("Database error, failed to create an account", err)
+        throw new Error("Failed to create account")
+    }
+    redirect('/dashboard')
 }
