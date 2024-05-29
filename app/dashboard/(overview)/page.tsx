20a96043681042d24239847c1fe0fca2b1cdf681
Jaarabytes <xavierandole78@gmail.com>
2024-05-29 16:14:12 +0300

    refactor: add `/dashboard` page
    add dashboard page with packages to buy

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,16 @@
-// export default
-
-
+import Packages from "@/app/ui/dashboard/Packages"
 export default function WelcomePage () {
     // This is the dashboard page
-
+    // Show them to pay
+    // Redesign db such that it remembers users packages and if they have paid or not
+    // Add stripe, daraja and metamask payment options
     return (
         <>
             <div>
-                <h1>Welcome , you have successfully logged in!</h1>
+                <h1 className="text-green-900 font-bold text-3xl p-5">Welcome , you have successfully logged in!</h1>
+                {/* Check if user has paid */}
+                <h1 className="text-green-900 font-bold text-xl px-5">Choose wisely</h1>
+                <Packages />
             </div>
         </>
     )
