bfd0453c147400f110d38e08db85f2ab4a45cedf
Jaarabytes <xavierandole78@gmail.com>
2024-06-20 22:46:08 +0300

    fix: removing all possible auth0 instances
    realized that auth0 is very expensive when applications scale (just like
    Vercel), removing everything

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,14 +1,9 @@
 import { getUser } from "@/app/lib/data"
-import { useUser } from "@auth0/nextjs-auth0/client"
 
 export default function ProfilePage () {
-    const { user, error , isLoading } = useUser();
-    if (isLoading) return <div>Loading ...</div>
-    if (error) return <div>{error.message}</div>
     return (
-        user && (
             <>
-            <h1 className="text-green-900 font-bold text-2xl">Hello, {user.email}</h1>
+            <h1 className="text-green-900 font-bold text-2xl">Hello insertEmailHERE</h1>
             <p className="text-green-900">We thank you for using our services and being a content customer</p>
             <div className="my-5">
                 <p className="mt-10 mb-3">Any complaints?</p>
@@ -18,5 +13,4 @@ export default function ProfilePage () {
              <button className="bg-green-900 hover:bg-green-700 text-white my-5 rounded-lg p-3">Submit</button>
         </>
         )
-    )
 }
