03061e43bc240abef6b4975e5a411035d38cac19
Jaarabytes <xavierandole78@gmail.com>
2024-06-21 15:28:36 +0300

    style: add black text for betetr visibility
    Add better visibility by making the `Loading` black using tailwind CSS
    classes

--- app/ui/Loading.tsx
+++ app/ui/Loading.tsx
@@ -9,7 +9,7 @@ const LoadingModal = ({ isOpen }: LoadingModalProps) => {
     return isOpen ? (
       <div className="modal fixed z-50 inset-0 overflow-y-auto bg-opacity-50 backdrop-blur-sm">
         <div className="modal-content flex items-center justify-center min-h-screen">
-            <p className="text-3xl">Loading...</p>
+            <p className="text-3xl text-black">Loading...</p>
         </div>
       </div>
     ) : null;
