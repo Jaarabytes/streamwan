78bbdae81479b855cd066081d3386527a24b16bc
Jaarabytes <xavierandole78@gmail.com>
2024-06-21 14:10:20 +0300

    fix: update imports for home page
    update imports for home page after moving components in the
    `/app/ui/home` directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 // This should hold multiple components similar to App.jsx, thank you
 
-import Footer from "@/app/ui/Footer";
-import Intro from "@/app/ui/Intro";
-import Marketing from "@/app/ui/Marketing";
-import Reviews from "@/app/ui/Reviews";
-import Finalize from "@/app/ui/Finalize";
+import Footer from "@/app/ui/home/Footer";
+import Intro from "@/app/ui/home/Intro";
+import Marketing from "@/app/ui/home/Marketing";
+import Reviews from "@/app/ui/home/Reviews";
+import Finalize from "@/app/ui/home/Finalize";
 
 export default function HomePage () {
 
