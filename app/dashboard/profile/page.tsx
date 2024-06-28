59104ba9f31a323e1998a96e4d24c19f98e22b66
Jaarabytes <xavierandole78@gmail.com>
2024-06-28 17:49:10 +0300

    style: add more to contribute
    add contributions for developers and more.

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -14,6 +14,13 @@ export default function ProfilePage () {
             <br /><br />
             <button type='submit' className="bg-green-900 hover:bg-green-700 text-white my-3 px-5 rounded-lg p-3"><a href='mailto:xh3rking96@gmail.com'>Submit</a></button>
             </form>
+            <p className='my-3'>Are you a software developer? That's even better.</p>
+            <p className='my-3'>Fork the github repository using the below link, fix it yourself and submit a pull request</p>
+            <p className='my-3'>You're submissions are highly welcomed</p>
+            <a href='https://github.com/Jaarabytes/streamwan/issues' className='text-green-900 font-bold' target='_blank'>https://github.com/Jaarabytes/streamwan</a>
+            <p className='my-3'>You can also raise issues or any complaints here: </p>
+            <a href='https://github.com/Jaarabytes/streamwan/issues' className='text-green-900 font-bold' target='_blank'>https://github.com/Jaarabytes/streamwan/issues</a>
+            <hr />
         </>
         )
 }
