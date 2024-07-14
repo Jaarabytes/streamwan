f6ed3eb647bfffb13fb0bfdca5e56e65dd7be39c
Jaarabytes <xavierandole78@gmail.com>
2024-07-14 15:12:35 +0300

    fix: will implement these later
    To be done later, i promise

---
+++ auth.ts
@@ -0,0 +1,12 @@
+// import { authConfig } from './auth.config'
+// import NextAuth from 'next-auth';
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaAdapter } from "@auth/prisma-adapter"
+
+
+// export const { signIn, signOut, auth, handlers } = NextAuth({
+//     adapter: PrismaAdapter(prisma),
+//     session: { strategy: "jwt" },
+//     ...authConfig
+// }
+// )
