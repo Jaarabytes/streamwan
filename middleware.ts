688c017966f01088dbfaeaad0a8fcfd50749ba6f
Jaarabytes <xavierandole78@gmail.com>
2024-07-16 16:15:18 +0300

    fix: remove stupid font
    Remove stupid font, i really hate that font

--- middleware.ts
+++ middleware.ts
@@ -5,4 +5,4 @@ export async function middleware(request: NextRequest) {
   return await updateSession(request);
 }
 // All routes containing /dashboard are blocked for unauthenticated users
-export const config = { matcher : [ '/dashboard:path*' ] }
+// export const config = { matcher : [ '/dashboard:path*' ] }
